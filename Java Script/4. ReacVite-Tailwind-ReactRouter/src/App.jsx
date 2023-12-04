888caac79ad72c393c0889dfc6d8266f3ea80f7d
Dwi Aji Cahyono <dwiaji724@gmail.com>
2023-12-04 13:11:40 +0700

    add a 4.

---
+++ Java Script/4. ReacVite-Tailwind-ReactRouter/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><h1 className="text-3xl font-bold underline text-red-500 justify-center items-center flex w-full h-full">
+    Hello world!
+  </h1></div>
+  )
+}
+
+export default App
